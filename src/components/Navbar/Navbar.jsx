f305175ffb26006a3fafca343b7455ffc00db97b
Vadym <vamed8926@gmail.com>
2020-05-04 10:49:24 +0300

    NovLink вместо <a>
    Добавил npm i rect-router-dom -save
    -save: для сохранения в сomit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from "./Navbar.module.css";
+import { NavLink } from 'react-router-dom';
 // let c1 = "item";
 // let c2 = "active";
 //  "item active"
@@ -11,19 +12,19 @@ const Navbar =() => {
     
     
     <div className={s.item}>
-      <a href="/Profile">Profile</a>
+      <NavLink to="/Profile" activeClassName={s.active}>Profile</NavLink>
     </div>
-    <div className={`${s.item} ${s.active}`}>  {/* это для пример */}
-      <a href="/Dialogs">Messages</a>
-    </div>
-    <div className={s.item}>
-      <a href="/News">News</a>
+    <div className={s.item}>  {/* это для пример */}
+      <NavLink to="/Dialogs" activeClassName={s.active}>Messages</NavLink>
     </div>
     <div className={s.item}>
-    <a href="/Music">Music</a>
+      <NavLink to="/News" activeClassName={s.active}>News</NavLink>
     </div>
     <div className={s.item}>
-      <a href="/Settings">Settings</a>
+    <NavLink to="/Music" activeClassName={s.active}>Music</NavLink>
+    </div>
+    <div className={s.item}>
+      <NavLink to="/Settings" activeClassName={s.active}>Settings</NavLink>
     </div>
   </nav>
 }

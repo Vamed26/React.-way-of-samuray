b6de78bbe59766febfdc2652d5e6bc3a15db75d7
Vadym <vamed8926@gmail.com>
2020-05-10 23:08:52 +0300

    leson24

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,22 +16,39 @@ const Message =(props) => {
 }
 
 const Dialogs =(props) => {
+  let dialogData= [
+    {id:1, name:"Vasya"},
+    {id:2, name:"Марина"},
+    {id:3, name:"Vitaliy"},
+    {id:4, name:"Ignat"},
+    {id:5, name:"Artem"},
+  ]
+  let messageData= [
+    {id:1, message:"Hello"},
+    {id:2, message:"How is your React"},
+    {id:3, message:"Hi"},
+    {id:4, message:"Yo"},
+    {id:5, message:"Yo-Yo"},
+  ]
     return  (
       <div className={s.dialogs}>
         <div className={s.DialogItem}>
 
-          <DialogItem name="Vasya" id="1"/>
-          <DialogItem name="Марина" id="2"/>
-          <DialogItem name="Vitaliy" id="3"/>
-          <DialogItem name="Ignat" id="4"/>
-          <DialogItem name="Artem" id="5"/>
+          <DialogItem name={dialogData[0].name} id={dialogData[0].id}/>
+          <DialogItem name={dialogData[1].name} id={dialogData[1].id}/>
+          <DialogItem name={dialogData[2].name} id={dialogData[2].id}/>
+          <DialogItem name={dialogData[3].name} id={dialogData[3].id}/>
+          <DialogItem name={dialogData[4].name} id={dialogData[4].id}/>
+          
           </div>
           
 
         <div className={s.messages}>
-          <Message message="Hello"/>
-          <Message message="How is your React"/>
-          <Message message="Yo"/> 
+          <Message message={messageData[0].message} id={messageData[0].id}/>
+          <Message message={messageData[1].message} id={messageData[1].id}/>
+          <Message message={messageData[2].message} id={messageData[2].id}/>
+          <Message message={messageData[3].message} id={messageData[3].id}/>
+          <Message message={messageData[4].message} id={messageData[4].id}/>
       </div>
       </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,24 +3,32 @@ import s from "./MyPosts.module.css";
 import Post from './Post/Post';
 
 const MyPosts = () => {
-    return <div>
+    let postData= [
+        {id: 1, message:"Hello gays", like: 26},
+        {id: 2, message:"It's my first post", like: 15},
+    
+      ]
+    return <div> 
 
         <div className={s.bird}>
+
+            
             <img src="https://assets.jpegmini.com/user/images/slider_puffin_before_mobile.jpg" />
+        </div>
+        <div className={s.postBlock}>
+            <h3>My Posts</h3>
         </div>
         <div className={s.NewPosts}>
             <textarea className={s.text}></textarea>
             <div>
-                
+
                 <button>Add post</button>
                 <button>Remove</button>
             </div>
         </div>
-        <div><h4>My Posts</h4>
-        </div>
-        <Post message= "Hello gays"  like='24' />
-        <Post message= "It's my first post" like='5'/>
-        
+        <Post message={postData[0].message} like={postData[0].like}/>
+        <Post message={postData[1].message} like={postData[1].like}/>
+
     </div>
 }
 export default MyPosts;
